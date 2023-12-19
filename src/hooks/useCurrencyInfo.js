a26a9a1b71a350774dce606869eb370479273885
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-19 17:37:10 +0530

    ::custom Hook ⚓

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
 
-  const [data,setData] = useStatetate({});
+  const [data,setData] = useState({});
   useEffect(() => {
     fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
       .then((res) => res.json())
-      .then((res) => setData(res.currency))
+      .then((res) => setData(res[currency]))
   } , [currency])
 
    console.log(data)
